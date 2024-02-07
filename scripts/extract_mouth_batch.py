6fcbb1f23e4155b491ee6b3efa28f5c63d653b7a
miftanurfarid <miftanurfarid@gmail.com>
2024-02-08 05:24:32 +0800

    fix print()

--- scripts/extract_mouth_batch.py
+++ scripts/extract_mouth_batch.py
@@ -41,7 +41,7 @@ def find_files(directory, pattern):
                 yield filename
 
 for filepath in find_files(SOURCE_PATH, SOURCE_EXTS):
-    print "Processing: {}".format(filepath)
+    print("Processing: {}".format(filepath))
     video = Video(vtype='face', face_predictor_path=FACE_PREDICTOR_PATH).from_video(filepath)
 
     filepath_wo_ext = os.path.splitext(filepath)[0]
@@ -50,5 +50,5 @@ for filepath in find_files(SOURCE_PATH, SOURCE_EXTS):
 
     i = 0
     for frame in video.mouth:
-    	io.imsave(os.path.join(target_dir, "mouth_{0:03d}.png".format(i)), frame)
-    	i += 1
+        io.imsave(os.path.join(target_dir, "mouth_{0:03d}.png".format(i)), frame)
+        i += 1
